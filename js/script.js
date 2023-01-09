0eda675671d0ff883e447c961df7a4879f170355
aliya0212 <120478758+aliya0212@users.noreply.github.com>
2023-01-09 19:55:46 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,101 @@
+
+// let value=250;
+// value=150;
+// confirm(value);
+
+
+// const FirstName='Aliya';
+// const SecondName='Oroskulova';
+// // 
+// const great=`oh,hi ${FirstName}`;
+// console.log(great);
+// const Name=prompt('Введите слово?')
+// alert(`В этом слове ${Name.length} букв`)
+
+
+
+// const Num=15%3;
+// alert(Num)
+
+
+
+// const Bit=15*1024*8;
+// alert(Bit);
+
+
+// 
+
+
+ // const Text=prompt('Ты ботик?');
+ // alert('Хаха лили слитая ботиха' )
+
+
+
+// const Num=45*5-200;
+// alert(Num);
+
+
+// const Mil=prompt('Укажите мили');
+// const Km=1.6*Mil
+// alert(+Km.toFixed(2));
+
+
+
+
+// let value;
+// let double=typeof(typeof(value));
+// alert(double)
+
+
+
+
+
+   // const Text=prompt('Укажите число');
+     // const Te=10<Text;
+    // alert(Te)
+
+
+// const Text=prompt('Укажите число');
+// const Te=20>Text;
+// alert(Te);
+
+
+
+// const Num=prompt('Укажите число');
+// const Per=(Num>=10)&&(Num<100)||(Num<-10);
+// alert(Per);
+
+
+
+
+// const Text=prompt('Сколько вам лет?');
+// if (Text<18) {
+//     alert('Too young');
+// } else if (Text>75) {
+//     alert('Too old')
+// } else if ((Text>=18) && (Text<=75)) {
+//     alert('Access granted!')
+// } else {
+//     alert('eror 909')
+// }
+
+
+
+
+
+// const Text=prompt('М или Ж');
+// if (Text=="М") {
+//     alert('Вы мужчина');
+// } else if (Text=="Ж") {
+//     alert('Вы женщина');
+// } else { 
+//    alert('Ошибка');
+// }
+
+
+дз// const Text=prompt('М или Ж');
+// if (Text=="М" ? alert('Вы мужчина'):alert('Вы женщина'));
+
+const Text=prompt('Цена');
+if (cost>500 ? alert('Высокая цена'):alert('Низкая цена'));
+
